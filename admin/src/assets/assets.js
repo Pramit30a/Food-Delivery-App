084f68926f17ed7ae234f6f94a97c89230a5fa97
Pramit Kumar Mandal <111217469+Pramit30a@users.noreply.github.com>
2024-09-19 22:29:52 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-app-backend-38h6.onrender.com/'
+export const url = 'https://food-delivery-app-backend-38h6.onrender.com'
